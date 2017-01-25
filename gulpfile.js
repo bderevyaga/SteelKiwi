1d58753ffacacd6ff24a252550e3e8fc4c144139
bderevyaga <bogdan.derevyaga@gmail.com>
2017-01-25 02:43:34 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    livereload = require('gulp-livereload'),
+    autoprefixer = require('gulp-autoprefixer'),
+    connect = require('gulp-connect');
+
+gulp.task('default', ['connect', 'watch']);
+
+gulp.task('connect', function () {
+    connect.server({
+        root: 'public',
+        livereload: true
+    });
+});
+
+gulp.task('sass', function () {
+    gulp.src('sass/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('public/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('pug', function () {
+    gulp.src('pug/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('public'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.sass', ['sass']);
+    gulp.watch('pug/**/*.pug', ['pug']);
+});
